d0faf9b78414d6d215202b5cd92831f81a79bdd3
Kobby-Jones <kobbyj099@gmail.com>
2022-10-28 18:37:12 -0700

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+var button1 = document.querySelector('#products .products-container .product-1 button');
+var button2 = document.querySelector('#hero button');
+button1.addEventListener('click', function (){
+    return alert("hello Button 1")
+})
+button2.addEventListener("click", function () {
+    return alert("Hello Button 2");
+})
